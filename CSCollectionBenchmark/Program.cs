6557a523870e6e228a2d4c4b38dcf93cad8c47a5
arba <arba.jtk10@gmail.com>
2022-11-26 23:19:44 +0700

    IEnumerable benchmark with list vs yield return

---
+++ CSCollectionBenchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace CSCollectionBenchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<YieldIEnumerableBenchmark>();
+        }
+    }
+}
---
+++ CSCollectionBenchmark/YieldIEnumerableBenchmark.cs
@@ -0,0 +1,48 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace CSCollectionBenchmark
+{
+    [MemoryDiagnoser]
+    public class YieldIEnumerableBenchmark
+    {
+        readonly int total = 1_000;
+
+        [Benchmark]
+        public void PrintFruit()
+        {
+            Console.WriteLine("Using GetFruit");
+            foreach (var fruit in GetFruit(total))
+            {
+                Console.WriteLine(fruit);
+            }
+        }
+        
+        [Benchmark]
+        public void PrintFruitYield()
+        {
+            Console.WriteLine("Using GetFruitYield");
+            foreach (var fruit in GetFruitYield(total))
+            {
+                Console.WriteLine(fruit);
+            }
+        }
+
+        public static IEnumerable<string> GetFruit(int total)
+        {
+            List<string> fruits = new();
+            for (int i = 0; i < total; i++)
+            {
+                fruits.Add("Fruit " + i);
+            }
+            return fruits;
+        }
+
+        public static IEnumerable<string> GetFruitYield(int total)
+        {
+            for (int i = 0; i < total; i++)
+            {
+                yield return "Fruit " + i;
+            }
+        }
+    }
+}
